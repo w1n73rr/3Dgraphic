9fd4a2a41a446d5f32fa5ae1998f1b5f22466ef9
w1n73rr <dushakov2005@gmail.com>
2024-11-28 00:37:18 +0400

    Добавьте файлы проекта.

---
+++ 3Dgraphic/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace _3Dgraphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picbox = new PictureBox();
+            tb_RR = new TextBox();
+            tb_A = new TextBox();
+            tb_B = new TextBox();
+            trackbar_movement = new TrackBar();
+            trackbar_turn_x = new TrackBar();
+            trackbar_turn_y = new TrackBar();
+            trackbar_turn_z = new TrackBar();
+            trackbar_size = new TrackBar();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)picbox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_movement).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_turn_x).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_turn_y).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_turn_z).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_size).BeginInit();
+            SuspendLayout();
+            // 
+            // picbox
+            // 
+            picbox.Location = new Point(12, 12);
+            picbox.Name = "picbox";
+            picbox.Size = new Size(776, 426);
+            picbox.TabIndex = 0;
+            picbox.TabStop = false;
+            picbox.Click += pictureBox1_Click;
+            // 
+            // tb_RR
+            // 
+            tb_RR.Location = new Point(185, 461);
+            tb_RR.Name = "tb_RR";
+            tb_RR.Size = new Size(125, 27);
+            tb_RR.TabIndex = 2;
+            tb_RR.Text = "0,1";
+            // 
+            // tb_A
+            // 
+            tb_A.Location = new Point(330, 461);
+            tb_A.Name = "tb_A";
+            tb_A.Size = new Size(125, 27);
+            tb_A.TabIndex = 3;
+            tb_A.Text = "0,5";
+            // 
+            // tb_B
+            // 
+            tb_B.Location = new Point(475, 461);
+            tb_B.Name = "tb_B";
+            tb_B.Size = new Size(125, 27);
+            tb_B.TabIndex = 4;
+            tb_B.Text = "0,1";
+            // 
+            // trackbar_movement
+            // 
+            trackbar_movement.Location = new Point(34, 494);
+            trackbar_movement.Maximum = 200;
+            trackbar_movement.Minimum = -200;
+            trackbar_movement.Name = "trackbar_movement";
+            trackbar_movement.Size = new Size(130, 56);
+            trackbar_movement.TabIndex = 5;
+            trackbar_movement.Scroll += trackbar_movement_Scroll;
+            // 
+            // trackbar_turn_x
+            // 
+            trackbar_turn_x.Location = new Point(185, 494);
+            trackbar_turn_x.Maximum = 360;
+            trackbar_turn_x.Minimum = -360;
+            trackbar_turn_x.Name = "trackbar_turn_x";
+            trackbar_turn_x.Size = new Size(130, 56);
+            trackbar_turn_x.TabIndex = 6;
+            trackbar_turn_x.Scroll += trackbar_turn_x_Scroll;
+            // 
+            // trackbar_turn_y
+            // 
+            trackbar_turn_y.Location = new Point(325, 494);
+            trackbar_turn_y.Maximum = 360;
+            trackbar_turn_y.Minimum = -360;
+            trackbar_turn_y.Name = "trackbar_turn_y";
+            trackbar_turn_y.Size = new Size(130, 56);
+            trackbar_turn_y.TabIndex = 7;
+            trackbar_turn_y.Scroll += trackbar_turn_y_Scroll;
+            // 
+            // trackbar_turn_z
+            // 
+            trackbar_turn_z.Location = new Point(475, 494);
+            trackbar_turn_z.Maximum = 360;
+            trackbar_turn_z.Minimum = -360;
+            trackbar_turn_z.Name = "trackbar_turn_z";
+            trackbar_turn_z.Size = new Size(130, 56);
+            trackbar_turn_z.TabIndex = 8;
+            trackbar_turn_z.Scroll += trackbar_turn_z_Scroll;
+            // 
+            // trackbar_size
+            // 
+            trackbar_size.Location = new Point(632, 494);
+            trackbar_size.Maximum = 9;
+            trackbar_size.Minimum = 1;
+            trackbar_size.Name = "trackbar_size";
+            trackbar_size.Size = new Size(130, 56);
+            trackbar_size.TabIndex = 9;
+            trackbar_size.Value = 1;
+            trackbar_size.Scroll += trackbar_size_Scroll;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(668, 459);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 10;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 537);
+            Controls.Add(button1);
+            Controls.Add(trackbar_size);
+            Controls.Add(trackbar_turn_z);
+            Controls.Add(trackbar_turn_y);
+            Controls.Add(trackbar_turn_x);
+            Controls.Add(trackbar_movement);
+            Controls.Add(tb_B);
+            Controls.Add(tb_A);
+            Controls.Add(tb_RR);
+            Controls.Add(picbox);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)picbox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_movement).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_turn_x).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_turn_y).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_turn_z).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackbar_size).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picbox;
+        private TextBox tb_RR;
+        private TextBox tb_A;
+        private TextBox tb_B;
+        private TrackBar trackbar_movement;
+        private TrackBar trackbar_turn_x;
+        private TrackBar trackbar_turn_y;
+        private TrackBar trackbar_turn_z;
+        private TrackBar trackbar_size;
+        private Button button1;
+    }
+}
---
+++ 3Dgraphic/Form1.cs
@@ -0,0 +1,227 @@
+﻿namespace _3Dgraphic
+{
+    public partial class Form1 : Form
+    {
+
+
+        private double angleX = 0, angleY = 0, angleZ = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private double  R = 0;
+        private double r = 0;
+        private double A = 0;
+        private double B = 0;
+        int dl = 0;
+        double degree_a = 0;
+        double degree_b = 0;
+        double degree_c = 0;
+        int measure = 1;
+
+        public void Draw_Spiral()
+        {
+            // Считываем данные
+            r = double.Parse(tb_RR.Text);  // Малый радиус
+            A = double.Parse(tb_A.Text);  // Начальный радиус
+            B = double.Parse(tb_B.Text);  // Постоянная
+            dl = trackbar_movement.Value;  // Дистанция
+            degree_a = trackbar_turn_x.Value * Math.PI / 180;  // Поворот по оси X
+            degree_b = trackbar_turn_y.Value * Math.PI / 180;  // Поворот по оси Y
+            degree_c = trackbar_turn_z.Value * Math.PI / 180;  // Поворот по оси Z
+            measure = trackbar_size.Value;  // Масштаб
+
+            // Стартовые значения рисунка
+            Bitmap mbit = new Bitmap(picbox.Width, picbox.Height);
+            Graphics gr = Graphics.FromImage(mbit);
+            gr.Clear(Color.Black);
+            Pen MyPen = new Pen(Color.White);
+
+            // Количество точек
+            int numPoints = 20;
+            double[] spiral_x = new double[numPoints];
+            double[] spiral_y = new double[numPoints];
+            double[] spiral_z = new double[numPoints];
+
+            // Начальные углы
+            double alpha = 0;
+            double beta = -2 * Math.PI;
+
+            // Шаг изменения углов
+            double dalpha = Math.PI / 360;  // Шаг для alpha
+            double dbeta = Math.PI / 4; // Шаг для beta
+
+            // Генерация точек спирали
+            for (int i = 0; i < numPoints; i++)
+            {
+                R = A + B * beta;  // Вычисление радиуса
+
+                // Координаты в пространстве
+                spiral_x[i] = (R + r * Math.Cos(alpha)) * Math.Sin(beta);
+                spiral_y[i] = (R + r * Math.Cos(alpha)) * Math.Cos(beta);
+                spiral_z[i] = r * Math.Sin(alpha);
+
+                // Обновление углов
+                alpha += dalpha;
+                beta += dbeta;
+            }
+
+            // Аффинные преобразования (масштабирование, повороты, смещение)
+            for (int i = 0; i < numPoints; i++)
+            {
+                double sina = Math.Sin(degree_a);
+                double sinb = Math.Sin(degree_b);
+                double sinc = Math.Sin(degree_c);
+                double cosa = Math.Cos(degree_a);
+                double cosb = Math.Cos(degree_b);
+                double cosc = Math.Cos(degree_c);
+
+                double x = spiral_x[i];
+                double y = spiral_y[i];
+                double z = spiral_z[i];
+
+                // Преобразование координат
+                spiral_x[i] = (x * cosb * cosc + y * sina * sinb * cosc - y * sinc * cosa + z * sinc * sina + z * sinb * cosa * cosc) * measure * 10;
+                spiral_y[i] = ((x * sinc * cosb + y * cosa * cosc + y * sinb * sina * sinc - z * sina * cosc + z * sinb * sinc * cosa) * measure * 10) + dl + 263;
+                spiral_z[i] = (-x * sinb + y * cosb * sina + z * cosa * cosb) * measure * 10 + 159;
+            }
+
+            // Рисуем линии между точками
+            for (int i = 0; i < numPoints - 1; i++)
+            {
+                int screenX1 = Convert.ToInt32(spiral_y[i]);
+                int screenY1 = Convert.ToInt32(spiral_z[i]);
+
+                int screenX2 = Convert.ToInt32(spiral_y[i + 1]);
+                int screenY2 = Convert.ToInt32(spiral_z[i + 1]);
+
+                int circleRadius = 10;
+                int numCirclePoints = 36;
+
+                bresenharmCircle(screenX1, screenY1, circleRadius, mbit);
+                bresenharmCircle(screenX2, screenY2, circleRadius, mbit);
+
+                List<Point> points1 = GetCirclePoints(screenX1, screenY1, circleRadius, numCirclePoints);
+                List<Point> points2 = GetCirclePoints(screenX2, screenY2, circleRadius, numCirclePoints);
+
+                // Соединяем соответствующие точки двух окружностей
+                for (int j = 0; j < numCirclePoints; j++)
+                {
+                    int nextIndex = (j + 1) % numCirclePoints; // Для замыкания окружности
+
+                    // Соединяем точки между окружностями
+                    gr.DrawLine(MyPen, points1[j], points2[j]);
+
+                    // Соединяем соседние точки в каждой окружности
+                    gr.DrawLine(MyPen, points1[j], points1[nextIndex]);
+                    gr.DrawLine(MyPen, points2[j], points2[nextIndex]);
+                }
+            }
+
+            // Отображение результата
+            picbox.Image = mbit;
+            picbox.Invalidate();
+        }
+
+        public List<Point> GetCirclePoints(int centerX, int centerY, int radius, int numPoints)
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < numPoints; i++)
+            {
+                double angle = 2 * Math.PI * i / numPoints; // Угол текущей точки
+                int x = centerX + (int)(radius * Math.Cos(angle));
+                int y = centerY + (int)(radius * Math.Sin(angle));
+                points.Add(new Point(x, y));
+            }
+            return points;
+        }
+
+        public void draw8Pixels(int x, int y, int x0, int y0, Color color, Bitmap map)
+        {
+            map.SetPixel(x + x0, y + y0, color);
+            map.SetPixel(x + x0, -y + y0, color);
+            map.SetPixel(-x + x0, y + y0, color);
+            map.SetPixel(-x + x0, -y + y0, color);
+            map.SetPixel(y + x0, x + y0, color);
+            map.SetPixel(y + x0, -x + y0, color);
+            map.SetPixel(-y + x0, x + y0, color);
+            map.SetPixel(-y + x0, -x + y0, color);
+        }
+        //Алгоритм Брезенхама рисования окружности
+        public void bresenharmCircle(int x0, int y0, int R, Bitmap map)
+        {
+            int x = 0; int y = R; int d = 3 - 2 * R;
+            while (y >= x)
+            {
+                draw8Pixels(x, y, x0, y0, Color.White, map);
+                if (d <= 0) d = d + 4 * x + 6;
+                else
+                {
+                    d = d + 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+            }
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            angleX += 0.01;
+            angleY += 0.01;
+            Draw_Spiral();
+        }
+        private void trackbar_size_Scroll(object sender, EventArgs e)
+        {
+            Draw_Spiral();
+        }
+
+        private void trackbar_turn_Scroll(object sender, EventArgs e)
+        {
+            Draw_Spiral();
+        }
+
+        private void trackbar_movement_Scroll(object sender, EventArgs e)
+        {
+            Draw_Spiral();
+        }
+
+        private void trackbar_turn_y_Scroll(object sender, EventArgs e)
+        {
+            Draw_Spiral();
+        }
+
+        private void trackbar_turn_z_Scroll(object sender, EventArgs e)
+        {
+            Draw_Spiral();
+        }
+        private void trackbar_turn_x_Scroll(object sender, EventArgs e)
+        {
+            Draw_Spiral();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((tb_RR.Text != ""))
+            {
+                //Отображаем элементы преобразования
+
+
+                trackbar_movement.Value = 0;
+                trackbar_turn_x.Value = 0;
+                trackbar_turn_y.Value = 0;
+                trackbar_turn_z.Value = 0;
+                trackbar_size.Value = 5;
+
+                Draw_Spiral();
+
+
+            }
+        }
+    }
+}
